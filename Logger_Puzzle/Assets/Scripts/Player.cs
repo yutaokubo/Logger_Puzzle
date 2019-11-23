0e80da41398ba3c5ff3661bea040f580d9b6f48f
KPC-017A1644\K017A1644 <yutaokubo@i.softbank.jp>
2019-11-23 09:44:09 +0900

    プレイヤーを設置、マス目移動ができるようにした。

---
+++ Logger_Puzzle/Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float mapChipDistance;
+
+    private Vector3 moveX;
+    private Vector3 moveY;
+
+    private Vector3 moveTargetPosition;
+    private Vector3 movePreviousPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveX = new Vector3(mapChipDistance, 0, 0);
+        moveY = new Vector3(0, mapChipDistance, 0);
+
+        moveTargetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position == moveTargetPosition)
+        {
+            SetTargetPosition();
+        }
+        Move();
+    }
+
+    private void SetTargetPosition()
+    {
+        movePreviousPosition = moveTargetPosition;
+
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            moveTargetPosition = transform.position + moveY;
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            moveTargetPosition = transform.position - moveY;
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            moveTargetPosition = transform.position + moveX;
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            moveTargetPosition = transform.position - moveX;
+            return;
+        }
+    }
+
+    private void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveTargetPosition, moveSpeed * Time.deltaTime);
+    }
+}
