3071e947f31019762ee101c443563b6720968fe6
KPC-017A1644\K017A1644 <yutaokubo@i.softbank.jp>
2019-11-26 13:43:38 +0900

    マップチップとマップ管理用のクラスを追加。 マップチップは各種条件を付けれるように

---
+++ Logger_Puzzle/Assets/Scripts/MapChip.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapChip : MonoBehaviour
+{
+    private int[,] mapPosition;//マップ上での位置
+
+
+    enum MapChipType//マップチップのタイプ
+    {
+        Nomal,//通常、
+        Rock,//通れない
+        Hole,//穴
+        Rever,//川
+    }
+
+    MapChipType nowMapChipType;//現在のマップチップのタイプ
+
+    private bool isCanPlayerEnter;//プレイヤーが意図的に侵入できるか
+    private bool isCanPlayerAutoEnter;//プレイヤーが自動的に侵入できるか
+    private bool isGrowingTree;//木が生えているか
+    private bool isOnWood;//丸太が乗っているか
+    private bool isCanWoodEnter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// どこのマス目か設定
+    /// </summary>
+    /// <param name="pos">マップ上の位置</param>
+    public void SetMapPosition(int[,] pos)
+    {
+        mapPosition = pos;
+    }
+
+    /// <summary>
+    /// 番号から状態を変化させる
+    /// </summary>
+    /// <param name="num"></param>
+    public void SetMapChipType(int num)
+    {
+        switch(num)
+        {
+            case 0://通常地形
+                nowMapChipType = MapChipType.Nomal;
+                isCanPlayerEnter = true;
+                isCanPlayerAutoEnter = true;
+                isCanWoodEnter = true;
+                break;
+        }
+    }
+}
---
+++ Logger_Puzzle/Assets/Scripts/MapManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MapManager : MonoBehaviour
+{
+    private int[,] mapNumbers;//各マップチップの番号
+
+    private MapChip[,] mapChips;//マップチップ格納用
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
